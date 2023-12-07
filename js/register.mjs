2e36a29e0eef1c8fe03ba600a8ce7cac581d761b
Eamith <yoesam36962@stud.noroff.no>
2023-12-07 10:50:27 +0100

    added register.mjs

--- js/register.mjs
+++ js/register.mjs
@@ -1,13 +1,47 @@
+import { APIURL } from "./api.mjs";
+
 const name = document.querySelector("#name");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const confirmPassword = document.querySelector("#confirm-password");
 const registerForm = document.querySelector("#register-form");
 
-const baseURL = "https://api.noroff.dev/api/v1/social/auth/register";
+const baseURL = APIURL + "/social/auth/register";
 
 // event listener for form
-registerForm.addEventListener("submit", (e) => {
+registerForm.addEventListener("submit", register);
+
+/**
+ * function to test name
+ * @param {string} name - name of user
+ * @returns boolean - true or false if name passes test
+ */
+function validateName(name) {
+  // Define a regular expression pattern for valid names
+  var nameRegex = /^[a-zA-Z0-9_]+$/;
+
+  // Return the validation result
+  return nameRegex.test(name);
+}
+
+/**
+ * function to test email
+ * @param {string} email -this is email of the user
+ * @returns boolean - true or false if email passes test
+ */
+function validateEmail(email) {
+  // Define a regular expression pattern for valid email addresses
+  var emailRegex = /^[a-zA-Z0-9._-]+@(stud\.)?noroff\.no$/;
+
+  // Return the validation result
+  return emailRegex.test(email);
+}
+
+/**
+ * function to register a user
+ * @param {Object} e - event emitter with preventDefault() function
+ */
+function register(e) {
   e.preventDefault(); // prevent default refresh
   let nameVal = name.value.trim();
   let emailVal = email.value;
@@ -35,26 +69,10 @@ registerForm.addEventListener("submit", (e) => {
   })
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
+      //console.log(data);
       if (data?.errors) return alert(data.errors[0].message);
 
       if (data?.id) alert("successfully registered, login now!");
       window.location.href = "/index.html";
     });
-});
-
-function validateName(name) {
-  // Define a regular expression pattern for valid names
-  var nameRegex = /^[a-zA-Z0-9_]+$/;
-
-  // Return the validation result
-  return nameRegex.test(name);
-}
-
-function validateEmail(email) {
-  // Define a regular expression pattern for valid email addresses
-  var emailRegex = /^[a-zA-Z0-9._-]+@(stud\.)?noroff\.no$/;
-
-  // Return the validation result
-  return emailRegex.test(email);
 }
