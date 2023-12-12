878b92800628aafb2456ff7b5796ad4a46aacc59
Eamith <yoesam36962@stud.noroff.no>
2023-12-12 20:15:42 +0100

    update index.mjs

--- js/index.mjs
+++ js/index.mjs
@@ -3,7 +3,6 @@ import { APIURL } from "./api.mjs";
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const loginForm = document.querySelector("#login-form");
-const errorContainer = document.querySelector("#error-container");
 
 const baseURL = `${APIURL}/social/auth/login`;
 
@@ -18,14 +17,6 @@ function login(e) {
   e.preventDefault(); // prevent default refresh
   let emailVal = email.value;
   let passVal = password.value;
-
-  // Validate email domain
-  if (!isValidEmail(emailVal)) {
-    displayError(
-      "Invalid email address. You can only login with noroff.no or stud.noroff.no email."
-    );
-    return;
-  }
 
   const data = {
     email: emailVal,
@@ -60,23 +51,4 @@ function login(e) {
       console.error("Fetch error:", error.message);
       displayError("An error occurred. Please try again.");
     });
-}
-
-/**
- * The email value must be a valid stud.noroff.no or noroff.no email address.
- * @param {string} email - The email address to validate
- * @returns {boolean} - True if the email domain is valid, false otherwise
- */
-function isValidEmail(email) {
-  const validDomains = ["noroff.no", "stud.noroff.no"];
-  const domain = email.split("@")[1];
-  return validDomains.includes(domain);
-}
-
-/**
- * Display error message in the error container
- * @param {string} errorMessage - The error message to display
- */
-function displayError(errorMessage) {
-  errorContainer.innerHTML = `<p class="error-message">${errorMessage}</p>`;
 }
